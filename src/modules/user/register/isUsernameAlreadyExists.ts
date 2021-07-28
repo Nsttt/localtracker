229f2d87717ee689827cde08f88b6ac6cadf8354
Nestor <elnestinatorHD@gmail.com>
2021-07-28 18:40:47 +0200

    feat: input for register resolver & custom validator

---
+++ src/modules/user/register/isUsernameAlreadyExists.ts
@@ -0,0 +1,32 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+import { User } from '../../../entities/User'
+
+@ValidatorConstraint({ async: true })
+export class IsUsernameAlreadyExistConstraint
+  implements ValidatorConstraintInterface
+{
+  validate(username: string) {
+    return User.findOne({ where: { username } }).then((user) => {
+      if (user) return false;
+      return true;
+    });
+  }
+}
+
+export function IsUsernameAlreadyExist(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsUsernameAlreadyExistConstraint,
+    });
+  };
+}
