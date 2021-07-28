14cc60c66f73cb4d10ad7820f3825b83a61e8e11
Nestor <elnestinatorHD@gmail.com>
2021-07-28 18:39:41 +0200

    feat: user entitity

---
+++ src/entities/User.ts
@@ -0,0 +1,21 @@
+import { Field, ID, ObjectType } from 'type-graphql';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class User extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  name: string;
+
+  @Field()
+  @Column('text', { unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+}
