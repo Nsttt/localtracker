2f422660fafd71d9e4f07c59081a928ce7748ec6
Nestor <elnestinatorHD@gmail.com>
2021-08-04 19:23:49 +0200

    feat: me query for auth user

---
+++ packages/api/src/modules/user/Me.ts
@@ -0,0 +1,16 @@
+import { MyContext } from './../../types/MyContext';
+import { Ctx, Query, Resolver } from 'type-graphql';
+
+import { User } from '../../entities/User';
+
+@Resolver()
+export class MeResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() ctx: MyContext): Promise<User | undefined> {
+    if (!ctx.req.session!.userId) {
+      return undefined; // GQL Should cast null to undefined.
+    }
+
+    return User.findOne(ctx.req.session!.userId);
+  }
+}
