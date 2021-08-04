e47dd3c9963eb638bc3e70e2ac68ea4f46a3b88d
Nestor <elnestinatorHD@gmail.com>
2021-08-04 19:22:49 +0200

    feat: add redis connection and session config

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -5,34 +5,53 @@ import cors from 'cors';
 import { ApolloServer } from 'apollo-server-express';
 import { createConnection } from 'typeorm';
 import { buildSchema } from 'type-graphql';
+import connectRedis from 'connect-redis';
+
+import { redis } from './redis';
+import { MeResolver } from './modules/user/Me';
+import { LoginResolver } from './modules/user/Login';
 import { RegisterResolver } from './modules/user/CreateUser';
 
 export default async function main() {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver],
+    resolvers: [RegisterResolver, LoginResolver, MeResolver],
+    authChecker: ({ context: { req } }) => {
+      return !!req.session.userId;
+    },
   });
-  const server = new ApolloServer({ schema });
+  const server = new ApolloServer({
+    schema,
+    context: ({ req }: any) => ({ req }),
+  });
   await server.start();
 
   const app = express();
-  app.use(cors({
-    credentials: true,
-    origin: process.env.front_uri,
-  }))
-  app.use(session({
-    store: ,
-    name: 'qid',
-    secret: "test",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
-    }
-  }));
+  app.use(
+    cors({
+      credentials: true,
+      origin: 'https://localhost:3000',
+    }),
+  );
+
+  const RedisStore = connectRedis(session);
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis as any,
+      }),
+      name: 'qid',
+      secret: 'test',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365,
+      },
+    }),
+  );
 
   server.applyMiddleware({ app });
 
