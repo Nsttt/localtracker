5f244ad8200ebb8a0ca795f51bf5567fe5c7509c
Nestor <elnestinatorHD@gmail.com>
2021-08-11 17:59:32 +0200

    test: register tests

---
+++ packages/api/src/modules/user/register/Register.test.ts
@@ -0,0 +1,58 @@
+import { Connection } from 'typeorm';
+import faker from 'faker';
+
+import { gCall } from '../../../test-utils/gCall';
+import { testConn } from '../../../test-utils/testConn';
+import { User } from '../../../entities/User';
+
+let conn: Connection;
+beforeAll(async () => {
+  conn = await testConn();
+});
+afterAll(async () => {
+  await conn.close();
+});
+
+const registerMutation = `
+mutation Register($data: RegisterInput!) {
+    register(data: $data) {
+    id
+    name
+    username
+    }
+}
+`;
+
+describe('Register', () => {
+  it('Creates a user', async () => {
+    const user = {
+      name: faker.name.firstName(),
+      username: faker.internet.userName(),
+      password: faker.internet.password(),
+    };
+
+    const response = await gCall({
+      source: registerMutation,
+      variableValues: {
+        data: user,
+      },
+    });
+
+    expect(response).toMatchObject({
+      data: {
+        register: {
+          name: user.name,
+          username: user.username,
+        },
+      },
+    });
+
+    const userFromDB = await User.findOne({
+      where: {
+        username: user.username,
+      },
+    });
+    expect(userFromDB).toBeDefined();
+    expect(userFromDB!.name).toBe(user.name);
+  });
+});
