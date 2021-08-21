850db5bc7836c6d534520eb5eb633f805440a08d
Nestor <elnestinatorHD@gmail.com>
2021-08-21 18:05:39 +0200

    feat: login page

--- packages/web/pages/login.tsx
+++ packages/web/pages/login.tsx
@@ -1,7 +1,34 @@
+import { Container, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import type { NextPage } from "next";
+import Header from "../components/Header";
+import Form from "../components/LoginForm";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+}));
 
 const LogIn: NextPage = () => {
-  return <div>Hello</div>;
+  const classes = useStyles();
+
+  return (
+    <>
+      <Header />
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Form />
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default LogIn;
