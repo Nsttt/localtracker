cce2270983241b9ce019a379219a9a8f98fe73dc
Nestor <elnestinatorHD@gmail.com>
2021-08-23 12:56:16 +0200

    feat(web): login page tweaks

--- packages/web/pages/login.tsx
+++ packages/web/pages/login.tsx
@@ -1,17 +1,20 @@
-import { Container, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import type { NextPage } from "next";
-import Header from "../components/Header";
-import Form from "../components/LoginForm";
+import type { NextPage } from 'next';
+import { Container, Typography } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-}));
+import Header from '../components/Header';
+import Form from '../components/Form';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  }),
+);
 
 const LogIn: NextPage = () => {
   const classes = useStyles();
@@ -21,10 +24,10 @@ const LogIn: NextPage = () => {
       <Header />
       <Container component="main" maxWidth="xs">
         <div className={classes.paper}>
-          <Typography component="h1" variant="h5">
-            Sign in
+          <Typography component="h1" variant="h4">
+            Log in
           </Typography>
-          <Form />
+          <Form formType="login"/>
         </div>
       </Container>
     </>
