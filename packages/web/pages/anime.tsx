f2c880217379a5c471ad056e8ccb0bfaf4018e9e
Nestor <elnestinatorHD@gmail.com>
2021-09-02 22:31:55 +0200

    feat(web): anime list page still wip

--- packages/web/pages/anime.tsx
+++ packages/web/pages/anime.tsx
@@ -1,20 +1,86 @@
 import type { NextPage } from 'next';
-import { Container } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 import Header from '../components/Header/header';
 import { Card } from '../components/Card/card';
 
+const useAnimeStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      paddingRight: '7.5%',
+      paddingLeft: '3.3%',
+      flexGrow: 1,
+    },
+  }),
+);
+
 const AnimeList: NextPage = () => {
+  const classes = useAnimeStyles();
+
   return (
     <>
       <Header />
-      <Container component="main" maxWidth="xs">
-        <Card cardType="anime" />
-        <Card cardType="anime" />
-        <Card cardType="anime" />
-        <Card cardType="anime" />
-        <Card cardType="anime" />
-      </Container>
+      <Grid container className={classes.container}>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={2}>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+            <Grid item>
+              <Card cardType="anime" />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 };
