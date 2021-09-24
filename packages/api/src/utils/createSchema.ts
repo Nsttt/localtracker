dadee097c01fd486cb315638f6e2b450c5d43377
Nestor <elnestinatorHD@gmail.com>
2021-09-24 19:43:44 +0200

    feat(api): add anime and profile picture resolvers

--- packages/api/src/utils/createSchema.ts
+++ packages/api/src/utils/createSchema.ts
@@ -1,12 +1,20 @@
 import { buildSchema } from 'type-graphql';
-import { ProfilePictureResolver } from "../modules/user/ProfilePicture";
+import { ProfilePictureResolver } from '../modules/user/ProfilePicture';
 import { LoginResolver } from '../modules/user/Login';
 import { LogoutResolver } from '../modules/user/Logout';
 import { MeResolver } from '../modules/user/Me';
 import { RegisterResolver } from '../modules/user/CreateUser';
+import { AnimeResolver } from '../modules/anime/CreateAnime';
 
 export const createSchema = () =>
   buildSchema({
-    resolvers: [LoginResolver, LogoutResolver, RegisterResolver, MeResolver, ProfilePictureResolver],
+    resolvers: [
+      LoginResolver,
+      LogoutResolver,
+      RegisterResolver,
+      MeResolver,
+      ProfilePictureResolver,
+      AnimeResolver,
+    ],
     authChecker: ({ context: { req } }) => !!req.session.userId,
   });
