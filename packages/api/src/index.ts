739fdac7fb664ff341476c8f742dfec0bb6c6cbc
Nestor <elnestinatorHD@gmail.com>
2021-11-20 18:28:43 +0100

    chore: move code to index.ts

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,3 +1,58 @@
-import main from './app';
+import 'reflect-metadata';
+import express from 'express';
+import session from 'express-session';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+import { createConnection } from 'typeorm';
+import Redis from 'ioredis';
+import connectRedis from 'connect-redis';
+
+import { createSchema } from './utils/createSchema';
+
+async function main() {
+  await createConnection();
+
+  const schema = await createSchema();
+  const server = new ApolloServer({
+    schema,
+    context: ({ req, res }: any) => ({ req, res }),
+  });
+  await server.start();
+
+  const app = express();
+  app.use(
+    cors({
+      credentials: true,
+      origin: 'https://localhost:3000',
+    }),
+  );
+
+  const redis = new Redis();
+  const RedisStore = connectRedis(session);
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis as any,
+      }),
+      name: 'qid',
+      secret: 'test',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365,
+      },
+    }),
+  );
+
+  server.applyMiddleware({ app });
+
+  app.listen({ port: process.env.API_PORT || 4000 }, () => {
+    console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+  });
+
+  return { server, app };
+}
 
 main();
