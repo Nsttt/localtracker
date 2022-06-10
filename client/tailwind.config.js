a5dbd10cfd3b891d4cbb4dc1508dca3030ef14de
Nestor <elnestinatorHD@gmail.com>
2022-06-10 10:40:59 +0200

    Init client development with SolidJS

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
